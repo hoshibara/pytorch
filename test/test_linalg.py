041679a6e4c08a12d1d1cc7a196a373119214b52
Xingyuan Li <xingyuan.li@intel.com>
2026-08-15 23:57:24 +0800

    skip long time ut

--- test/test_linalg.py
+++ test/test_linalg.py
@@ -27,7 +27,7 @@ from torch.testing._internal.common_utils import \
     (TestCase, run_tests, TEST_SCIPY, IS_MACOS, IS_WINDOWS, slowTest,
      TEST_WITH_ROCM, IS_FBCODE, IS_REMOTE_GPU, iter_indices,
      make_fullrank_matrices_with_distinct_singular_values,
-     freeze_rng_state, IS_ARM64, IS_SANDCASTLE, TEST_OPT_EINSUM, isRocmArchAnyOf, parametrize, skipIfTorchDynamo,
+     freeze_rng_state, IS_ARM64, IS_SANDCASTLE, TEST_OPT_EINSUM, isRocmArchAnyOf, parametrize, skipIfTorchDynamo, skipIfXpu,
      skipIfRocmArch, skipIfRocmVersionAtLeast, setBlasBackendsToDefaultFinally, setLinalgBackendsToDefaultFinally, serialTest, skipIfRocm,
      runOnRocmArch, MI200_ARCH, MI300_ARCH, MI350_ARCH, NAVI_ARCH, TEST_CUDA,
      skipIfNoNvmath)
@@ -5987,6 +5987,7 @@ class TestLinalg(TestCase):
         self._test_lobpcg_method(device, dtype, 'basic')
 
     @skipCPUIfNoLapack
+    @skipIfXpu(msg="XPU takes too long to run this test")
     @dtypes(torch.double)
     def test_lobpcg_ortho(self, device, dtype):
         self._test_lobpcg_method(device, dtype, 'ortho')
